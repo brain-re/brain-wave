61c662751ba38f1acec6e00416d1a5c386fcb4f8
root <root@localhost.localdomain>
2022-02-19 03:04:01 +0100

    New commit message.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
 const sample = require("./sample");
+const product = require("./products")
 
 router.use("/api/sample", sample);
-router.use("/api/sample", sample);
+router.use("/api/products", product)
 
 module.exports = router;

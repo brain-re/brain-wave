1898b46291e9b883da57b17dbc1f52e207ed3085
VincentP-HANFF <vincent.philippe18@gmail.com>
2022-03-25 00:41:33 +0100

    Add debounce time for querying API

--- app/src/app/shared/services/product.service.ts
+++ app/src/app/shared/services/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { IProduct } from '../../logic/interfaces/product.interface';
-import { tap } from 'rxjs/operators';
+import { debounceTime, map, tap } from 'rxjs/operators';
 import { ISearchProduct } from 'src/app/features/product/product-container/product-list/form/product-search.type';
 
 const HTTP_API = '/api/';
@@ -12,8 +12,19 @@ const HTTP_API = '/api/';
 })
 export class ProductService {
   public products$: BehaviorSubject<IProduct[]> = new BehaviorSubject(null);
+  public subject = new Subject();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.subject.pipe(
+      debounceTime(1500),
+      map(search =>
+        this.http.get<IProduct[]>(`${HTTP_API}products?search=${search}`)
+        .subscribe((value) => {
+          this.products$.next(value);
+        })
+      )
+    ).subscribe();
+  }
 
   public fetchProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(`${HTTP_API}products`)
@@ -25,10 +36,6 @@ export class ProductService {
   }
 
   public searchProducts(searchProduct: ISearchProduct) {
-    const input_search = searchProduct.search;
-      this.http.get<IProduct[]>(`${HTTP_API}products?search=${input_search}`)
-      .subscribe((value) => {
-        this.products$.next(value);
-      });
+    this.subject.next(searchProduct.search);
   }
 }

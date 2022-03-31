5a627862ef9746c959bfe4933c29bed07358a411
5698741 <arnould.wilfried3@gmail.com>
2022-03-31 02:28:36 +0200

    add function display categories at /api/categories and create categories at /api/categories/create see discord for more informations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const product = require("./products")
+const product = require("./products");
+const categorie = require("./categories")
 
 router.use("/api/products", product)
+router.use("/api/categories", categorie)
 
 module.exports = router;

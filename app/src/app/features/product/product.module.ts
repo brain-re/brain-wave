97737a7f9da3cacaa71e8f3a87f3a0070fe51945
Vincent <vincent.philippe18@gmail.com>
2023-02-22 01:26:15 +0100

    feat: product details (#2)
    * init
    
    * Product details

--- app/src/app/features/product/product.module.ts
+++ app/src/app/features/product/product.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from '../../shared/modules/shared.module';
 import { ProductContainerComponent } from './product-container/product-container.component';
 import { ProductListComponent } from './product-container/product-list/product-list.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const APP_PRODUCT_ROUTES: Routes = [
   {
@@ -18,19 +19,23 @@ const APP_PRODUCT_ROUTES: Routes = [
     ],
   },
   {
-    path: 'products',
+    path: 'product',
     component: ProductContainerComponent,
     children: [
       {
         path: 'new',
         component: ProductCreateComponent
+      },
+      {
+        path: ':_id',
+        component: ProductDetailsComponent,
       }
     ]
   }
 ];
 
 @NgModule({
-  declarations: [ProductContainerComponent, ProductCreateComponent],
+  declarations: [ProductContainerComponent, ProductCreateComponent, ProductDetailsComponent],
   imports: [SharedModule, RouterModule.forChild(APP_PRODUCT_ROUTES)],
 })
 export class ProductModule {}

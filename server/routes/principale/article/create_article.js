f92495a7c45d03d6ee0ddb6b030bdc9f41f38b78
5698741 <arnould.wilfried3@gmail.com>
2023-04-26 20:25:13 +0200

    fix create article bug

--- server/routes/principale/article/create_article.js
+++ server/routes/principale/article/create_article.js
@@ -26,12 +26,13 @@ router.post("/", (req, res) => {
           name: req.body['name'].trim(),
           description: req.body['description'].trim(),
           price: req.body['price'].trim(),
-          categories: req.body['categories'].trim(),
+          category: req.body['categories'],
           creator: id_decoded.toString().trim(),
           images: req.body['images'].trim(),
           });
           create_article.save(function(err){
             if (err) {
+              console.log(err)
               res.send(400, '[!] all information are not send correctly to the server for the creation of a article, or this name already exist')
               res.end()
             }
